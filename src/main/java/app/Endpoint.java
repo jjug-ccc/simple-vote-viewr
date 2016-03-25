58739e6c84535689f9abc0188565094fe5c1f335
Taichi Uragami <backpaper0@gmail.com>
2016-03-26 01:49:05 +0900

    手抜きでいろいろとアレ

--- src/main/java/app/Endpoint.java
+++ src/main/java/app/Endpoint.java
@@ -1,25 +1,14 @@
 package app;
 
-import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonValue;
 import javax.json.stream.JsonGenerator;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.Link;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
 @Path("")
@@ -31,47 +20,22 @@ public class Endpoint {
     //        return null;
     //    }
 
+    private static final AtomicReference<List<Issue>> cache = new AtomicReference<>();
+
     @Path("issues")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public StreamingOutput get() {
-        Client client = ClientBuilder.newClient();
 
-        List<Issue> issues = new ArrayList<>();
-        Link link = Link
-                .fromUri("https://api.github.com/repos/jjug-ccc/call-for-paper-2016spring/issues")
-                .build();
-        do {
-            Response resp = client.target(link).request()
-                    .header(HttpHeaders.USER_AGENT, "voteviewer").get();
-            String json = resp.readEntity(String.class);
-            try (JsonReader reader = Json.createReader(new StringReader(json))) {
-                JsonArray array = reader.readArray();
-                for (JsonValue element : array) {
-                    JsonObject object = (JsonObject) element;
-                    String title = object.getString("title");
-                    int number = object.getInt("number");
-                    issues.add(new Issue(title, number));
-                }
-            } catch (RuntimeException e) {
-                throw new RuntimeException(json, e);
-            }
-            link = resp.getLink("next");
-        } while (link != null);
-
-        List<Reaction> reactions = issues.stream().map(issue -> {
-            String html = issue.getHtml(client);
-            Integer count = Reaction.findReaction(html);
-            return new Reaction(issue, count);
-        }).collect(Collectors.toList());
+        if (cache.get() == null) {
+            cache.compareAndSet(null, Issue.buildIssues());
+        }
 
         return out -> {
             try (JsonGenerator gen = Json.createGenerator(out)) {
                 gen.writeStartArray();
-                for (Reaction reaction : reactions) {
-                    gen.writeStartObject();
-                    reaction.writeJson(gen);
-                    gen.writeEnd();
+                for (Issue issue : cache.get()) {
+                    issue.writeJson(gen);
                 }
                 gen.writeEnd();
             }
--- src/main/java/app/Issue.java
+++ src/main/java/app/Issue.java
@@ -1,17 +1,30 @@
 package app;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import javax.json.stream.JsonGenerator;
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.HttpHeaders;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Issue {
 
     private final String title;
     private final Integer number;
+    private final Integer reaction;
 
-    public Issue(String title, Integer number) {
+    public Issue(String title, Integer number, Integer reaction) {
         this.title = title;
         this.number = number;
+        this.reaction = reaction;
     }
 
     public String getHtml(Client client) {
@@ -21,7 +34,38 @@ public class Issue {
     }
 
     public void writeJson(JsonGenerator gen) {
+        gen.writeStartObject();
         gen.write("title", title);
         gen.write("url", "https://github.com/jjug-ccc/call-for-paper-2016spring/issues/" + number);
+        if (reaction != null) {
+            gen.write("count", reaction);
+        } else {
+            gen.writeNull("count");
+        }
+        gen.writeEnd();
+    }
+
+    public static List<Issue> buildIssues() {
+        Client client = ClientBuilder.newClient();
+
+        List<Issue> issues = IntStream.rangeClosed(1, 40).mapToObj(number -> {
+            String url = "https://github.com/jjug-ccc/call-for-paper-2016spring/issues/" + number;
+            String html = client.target(url).request().header(HttpHeaders.USER_AGENT, "voteviewer")
+                    .get(String.class);
+            Document doc = Jsoup.parse(html);
+            String title = doc.select("span.js-issue-title").text();
+            Elements elements = doc.select("button.reaction-summary-item");
+            Integer reaction = null;
+            for (Element element : elements) {
+                Elements emoji = element.select("g-emoji");
+                if (Objects.equals(emoji.attr("alias"), "+1")) {
+                    emoji.remove();
+                    reaction = Integer.parseInt(element.text());
+                }
+            }
+            return new Issue(title, number, reaction);
+        }).collect(Collectors.toList());
+
+        return issues;
     }
 }
